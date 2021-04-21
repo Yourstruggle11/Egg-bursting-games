47217eb35451f53c161e9840a5edbb01412c40d4
Yourstruggle11 <82510209+Yourstruggle11@users.noreply.github.com>
2021-04-21 18:40:51 +0530

    bug fixed

--- main_game_page_style_and_js/game_code.js
+++ main_game_page_style_and_js/game_code.js
@@ -1,95 +1,62 @@
 $(document).ready(function () {
- 
-
     //creating counter     //codes for countdown
     var button = $("button");
     button.click(function () {
-
-    
         let time = 1.5;
         var SetTime = time * 60;
         var interval = setInterval(timer, 1000)
         function timer() {
             let min = Math.floor(SetTime / 60);
             let sec = SetTime % 60;
-
             //console.log(min);
             //console.log(sec);
-
             SetTime--;
-
             sec = sec < 10 ? "0" + sec : sec;
             min = min < 10 ? "0" + min : min;
-
             var countdown = document.getElementById("countdown");
             countdown.innerHTML = `Time Left : ${min}:${sec}`;
-            console.log(SetTime);
-
-
             if (min == 0 & sec == 0) {
                 clearInterval(interval);
                 setTimeout(function(){
                     alert("opps you lost the game! better luck next time")
                     window.location.href = "index.html";
                 },400)
-
             }
+                var count = 0;
+                click.click(function () {
+                    count++;
+                    if (count == 3) {
+                        setTimeout(function () {
+                            let name = prompt("Enter Your Name Gamer!", "");
 
-            /*var score =  prompt("Enter Your Name Gamer!");
-
-if(score != null){
-          console.log((1+SetTime) * (100/90) );
-      }
-      else{
-          window.location.href = "index.html";
-      }*/
-            //codes for showing scores
-
-            var count = 0;
-            click.click(function () {
-                count++;
-                if (count == 3) {
-                    setTimeout(function () {
-                        var name = prompt("Enter Your Name Gamer!");
-
-                        if (name != null) {
-                            alert("hello "+ name + " your score is : " + Math.floor((1+SetTime) * (100/90)));
-                            window.location.href = "index.html";
-                        }
-                        else {
-                            window.location.href = "index.html";
-                        }
-                    }, 400)
-                }
-            })
-        }
-
+                            if (name != null) {
+                                alert("hello "+ name + " your score is : " + Math.floor((1+SetTime) * (100/90)));
+                                window.location.replace("./index.html")
+                            }
+                            else {
+                                window.location.href = "./index.html";
+                            }
+                        }, 700)
+                    }
+                })
+            }
         //codes for start the games
         let dialogBox = $(".dialog_box");
         dialogBox.css("display", "none");
-
         //codes for start the animation
         let animation1, animation2, animation3;
-
         animation1 = $("#animation1");
         animation2 = $("#animation2");
         animation3 = $("#animation3");
-
         animation1.addClass("egg1_animation");
         animation2.addClass("egg2_animation");
         animation3.addClass("egg3_animation");
-
-
-
     })
-
     //codes for broke eggs
-
     var egg1 = $("#eggs1");
     var egg2 = $("#eggs2");
     var egg3 = $("#eggs3");
     var click = $(".click")
-
     egg1.click(function () {
         egg1.addClass("broken_egg")
     })
